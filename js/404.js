c5df279dfde3c7e14b09fad47c729c38d567e4a7
devyanigeca <devyanikul12345@gmail.com>
2022-01-01 16:12:00 +0530

    404 page added

---
+++ js/404.js
@@ -0,0 +1,11 @@
+/*
+VIEW IN FULL SCREEN MODE
+FULL SCREEN MODE: http://salehriaz.com/404Page/404.html
+
+DRIBBBLE: https://dribbble.com/shots/4330167-404-Page-Lost-In-Space
+*/
+$(document).ready(function(){
+    $('.brand-logo').click(function(e){
+        window.location.href = "/";
+    })
+})
